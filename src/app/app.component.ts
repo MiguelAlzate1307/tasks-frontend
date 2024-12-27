30ca0eaa9cb50cef5473d1265d49d250fb9cbf01
Miguel Alzate <alzatemiguel284@gmail.com>
2024-12-26 21:43:38 -0500

    Added navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
+import { AuthService } from './providers/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
+  readonly authSer = inject(AuthService);
+
   ngOnInit(): void {
     initFlowbite();
   }
---
+++ src/app/providers/auth.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly http = inject(HttpClient);
+  private readonly router = inject(Router);
+
+  isLogged() {
+    return !!localStorage.getItem('token');
+  }
+}
